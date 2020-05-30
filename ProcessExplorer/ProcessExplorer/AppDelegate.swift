30feb6ec80aeda90db7e379624b23212eff83a39
Aleksandr Kucherenko <gogypro@gmail.com>
2020-05-30 14:12:56 +0300

    Created sketch project

---
+++ ProcessExplorer/ProcessExplorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ProcessExplorer
+//
+//  Created by Aleksandr Kucherenko on 30.05.2020.
+//  Copyright © 2020 Aleksandr Kucherenko. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ ProcessExplorer/ProcessExplorer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProcessExplorer
+//
+//  Created by Aleksandr Kucherenko on 30.05.2020.
+//  Copyright © 2020 Aleksandr Kucherenko. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
