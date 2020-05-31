ca78617ffa377bd15e8d17c73404fa287dbf4784
Aleksandr Kucherenko <gogypro@gmail.com>
2020-05-31 19:54:36 +0300

    Embedded process details UI into scroll view.

---
+++ ProcessExplorer/ProcessExplorer/Misc/FlippedClipView.swift
@@ -0,0 +1,15 @@
+//
+//  FlippedClipView.swift
+//  ProcessExplorer
+//
+//  Created by Aleksandr Kucherenko on 31.05.2020.
+//  Copyright © 2020 Aleksandr Kucherenko. All rights reserved.
+//
+
+import Cocoa
+
+class FlippedClipView: NSClipView {
+	override var isFlipped: Bool {
+		return true
+	}
+}
